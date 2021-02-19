06389bd7666ec5d4e0eeb1d7bd98338a41561b7e
Tamtnguye <apple@Apples-MacBook-Pro.local>
2021-02-19 10:04:02 -0800

    new solution

--- src/components/option.js
+++ src/components/option.js
@@ -6,7 +6,7 @@ const Options = ({number, unSelectedCourseList,choicesid,change}) => {
   return (
     <div >
      <label htmlFor="set"> {number}</label>
-      <select onChange={change} name={choicesid} id={choicesid}>
+      <select onChange={change} name={choicesid} id={choicesid} disabled={disabled}>
         <option value="select">Please Select</option>
         {unSelectedCourseList(`${choicesid}`).map((course) => (
           <option key={course} value={course}>
