6832535222ad5be757a97979ef40b89e18cafd53
Tamtnguye <apple@Apples-MacBook-Pro.local>
2021-03-11 16:30:17 -0800

    march 11

--- src/App.js
+++ src/App.js
@@ -5,20 +5,20 @@ import axios from 'axios';
 
 
 //COMPONENTS
-import Header from "./components/Header"
-import Banner from "./components/Banner.js"
-import NavBar from "./components/NavBar.js"
-import Content from "./components/Content.js"
-import Titles from "./components/Titles.js"
-import Buttons from "./components/Buttons.js"
-import Footer from "./components/Footer"
+import Header from "./components/Header";
+import Banner from "./components/Banner.js";
+import NavBar from "./components/NavBar.js";
+import Content from "./components/Content.js";
+import Titles from "./components/Titles.js";
+import Buttons from "./components/Buttons.js";
+import Footer from "./components/Footer";
 
 //import Home from "./components/Home.js"
 
 
 //import Select from './Select';
 import FormSelect from "./components/Form";
-import Options from "./components/Option"
+import Options from "./components/Option";
 
 const courseList = ["Web & Mobile", "AI & Machine", "Programming Paradigms", "Technical Programming", "Cloud Computing (DTC)", "Predictive Analytics (DTC)", "Defer"]
 function App() {
