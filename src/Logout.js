e9fa207325e267e105af21e158a9f7ed4df995d5
Gurmeet Sandhu <gurmeet990@gmail.com>
2021-04-04 23:29:53 -0700

    validation, authentication done and able to download CSV

---
+++ src/Logout.js
@@ -0,0 +1,32 @@
+import { useState,useEffect } from "react";
+import './App.css';
+//COMPONENTS
+import Header from "./components/Header"
+import Banner from "./components/Banner.js"
+import NavBar from "./components/NavBar.js"
+
+import Footer from "./components/Footer"
+
+import axios from "axios";
+
+
+export default function Logout() {
+
+    useEffect(() => {
+        localStorage.removeItem('token')
+    })
+
+    return (
+        <div className="Home">
+            <Header />
+
+            <Banner />
+
+            <NavBar />
+            
+                <div>You are successfully logged out!</div> 
+
+            <Footer />
+        </div>
+    );
+}
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,11 +5,13 @@ function NavBar() {
     return (
         <div className="NavBar">
         <ul className="links">
-          <li><a href="">BTech</a></li>
+          <li><a href="/">Home</a></li>
           <li><a href="">Program Updates</a></li>
           <li><a href="">Careers</a></li>
           <li><a href="">Part-Time</a></li>
           <li><a href="">Full Time</a></li>
+          <li><a href="/login">Admin</a></li>
+          {localStorage.getItem('token') ? <li><a href="/logout">Logout</a></li> : <li></li>}
         </ul>
       </div>
     )
